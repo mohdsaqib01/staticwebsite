dbb092bb500b03d2c6dfc81ffbf7e58729c84656
Mohdsaqib <mohdsaqibshayar@gmail.com>
2023-11-01 18:22:29 +0530

    python basics

---
+++ common/data_structure.py
@@ -0,0 +1,19 @@
+name= 'mohammad saqib'
+# print(name[7])
+# print(name[1])
+# print(name[8])
+# print(name[3])
+# print(name[6])
+# print(name[:4])
+# print(name[5:])
+# x=[1,2,5,6,10,12,15]
+# a=x[:5:2]
+# print(a)
+# print(x[::2])
+# print(x[1::2])
+rev_name=[name[::-1]]
+print(rev_name)
+print(name[:8:-1])
+# or
+
+print(name[-1:8:-1])
---
+++ common/loops.py
@@ -0,0 +1,6 @@
+movies= ['pk','3idiots','dangal','jawan','pathan','kal ho na ho','mai hoon na','batman','race','luck','dhoom1','aashiqui','harry potter','msg','jaal, the trap']
+print(f'total movies={len(movies)}')
+for name in movies:
+    # print(name)
+    size=len(name)
+    print(f"{name} has {size} chars")
